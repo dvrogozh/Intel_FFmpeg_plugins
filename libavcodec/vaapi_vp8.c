60b21155cde54152ea47c12b549a814e46ed1c38
Jun Zhao <jun.zhao@intel.com>
2016-11-15 11:20:28 +0800

    lavc/vaapi_vp8: fix fate test break issue.
    Change the copy right with the name "FFmpeg", else
    break FATE test with "make fate"
    
    https://jira01.devtools.intel.com/browse/MN-417
    
    Signed-off-by: Jun Zhao <jun.zhao@intel.com>

--- libavcodec/vaapi_vp8.c
+++ libavcodec/vaapi_vp8.c
@@ -1,5 +1,5 @@
 /*
- * This file is part of ffmpeg.
+ * This file is part of FFmpeg.
  *
  * ffmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
