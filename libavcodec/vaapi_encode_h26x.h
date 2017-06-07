4f720ded1305441e7b0c7248bb35f6ea81c7ecbe
Leilei <leilei.shang@intel.com>
2017-06-07 16:10:27 +0800

    avcodec: Add set_ue_golomb_long() to support 32bits UE golomb
    This patch is to fix bug MN-4432
    https://jira01.devtools.intel.com/browse/MN-4432
    
    put_bits just support write up to 31 bits, when write 32 bit in
    put_bits, it's will overwrite the bit buffer, because the default
    assert level is 0, the av_assert2(n <= 31 && value < (1U << n))
    in put_bits can not be trigger runtime. Add set_ue_golomb_long()
    to support 32bits UE golomb.
    
    Change-Id: Ie13d04da87d4b36c56cbc32a2b3d702e8479128b
    Signed-off-by: Jun Zhao <jun.zhao@intel.com>
    Signed-off-by: Leilei <leilei.shang@intel.com>

--- libavcodec/vaapi_encode_h26x.h
+++ libavcodec/vaapi_encode_h26x.h
@@ -30,7 +30,7 @@
 // for tracing purposes.  Here, it is just discarded.
 
 #define write_u(pbc, width, value, name) put_bits(pbc, width, value)
-#define write_ue(pbc, value, name)       set_ue_golomb(pbc, value)
+#define write_ue(pbc, value, name)       set_ue_golomb_long(pbc, value)
 #define write_se(pbc, value, name)       set_se_golomb(pbc, value)
 
 #define u(width, ...) write_u(pbc, width, __VA_ARGS__)
