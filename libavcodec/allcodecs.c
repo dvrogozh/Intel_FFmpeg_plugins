cbcb5a33a02116626bce7e8f6aa4b3f781570c70
ChaoX A Liu <chaox.a.liu@intel.com>
2017-07-11 16:06:21 +0800

    configure: Fix bug that ffmpeg can't be built successfully without enabling libmfx. bug-id: MN-5288
    Signed-off-by: ChaoX A Liu <chaox.a.liu@intel.com>
    Change-Id: I3c83edc114170f8eaf3502709961d8bd3127a0bb

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -89,6 +89,7 @@ void avcodec_register_all(void)
     REGISTER_HWACCEL(HEVC_VAAPI,        hevc_vaapi);
     REGISTER_HWACCEL(HEVC_VDPAU,        hevc_vdpau);
     REGISTER_HWACCEL(MJPEG_CUVID,       mjpeg_cuvid);
+    REGISTER_HWACCEL(MJPEG_QSV,         mjpeg_qsv);
     REGISTER_HWACCEL(MPEG1_CUVID,       mpeg1_cuvid);
     REGISTER_HWACCEL(MPEG1_XVMC,        mpeg1_xvmc);
     REGISTER_HWACCEL(MPEG1_VDPAU,       mpeg1_vdpau);
