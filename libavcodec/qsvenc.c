2db9e5539c78bc58d851b3d605d834b3adc4b150
Huang, Zhengxu <zhengxu.huang@intel.com>
2017-08-05 00:02:06 +0800

    Align the VBV params with the x264

--- libavcodec/qsvenc.c
+++ libavcodec/qsvenc.c
@@ -376,7 +376,7 @@ static int init_video_param(AVCodecContext *avctx, QSVEncContext *q)
     q->param.mfx.NumSlice           = avctx->slices;
     q->param.mfx.NumRefFrame        = FFMAX(0, avctx->refs);
     q->param.mfx.EncodedOrder       = 0;
-    q->param.mfx.BufferSizeInKB     = q->buffer_size/1000;
+    q->param.mfx.BufferSizeInKB     = q->buffer_size/8000;
 
     desc = av_pix_fmt_desc_get(sw_format);
     if (!desc)
