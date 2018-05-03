3d6e8f566e39df2089638507b9542b452e18822e
Zhong Li <zhong.li@intel.com>
2018-05-03 17:34:14 +0800

    lavc/qsvenc: add an option to disable MFE mode
    Not convenient if using numerals to set MFE mode. It is ambiguous
    and misleading (e.g: user may misunderstand setting mfmode to 1 is to
    enable MFE but actually it is to disable MFE, and set it to be 5 or above is meaningless).
    
    MFX_MF_MANUAL hasn't been exposed since it is to be implemented.
    
    Signed-off-by: Zhong Li <zhong.li@intel.com>

--- libavcodec/qsvenc_h264.c
+++ libavcodec/qsvenc_h264.c
@@ -174,7 +174,9 @@ static const AVOption options[] = {
     { "mv_cost_sf",         "MV cost scaling ratio", OFFSET(qsv.mv_cost_sf), AV_OPT_TYPE_INT, { .i64 = 0 }, 0, 3, VE },
 
 #if QSV_HAVE_MF
-    { "mfmode", "Multi-Frame Mode", OFFSET(qsv.mfmode), AV_OPT_TYPE_INT, { .i64 = MFX_MF_AUTO }, 0, INT_MAX, VE },
+    { "mfmode", "Multi-Frame Mode", OFFSET(qsv.mfmode), AV_OPT_TYPE_INT, { .i64 = MFX_MF_AUTO }, MFX_MF_DEFAULT, MFX_MF_AUTO, VE, "mfmode"},
+    { "off"    , NULL, 0, AV_OPT_TYPE_CONST, { .i64 = MFX_MF_DISABLED }, INT_MIN, INT_MAX,     VE, "mfmode" },
+    { "auto"   , NULL, 0, AV_OPT_TYPE_CONST, { .i64 = MFX_MF_AUTO     }, INT_MIN, INT_MAX,     VE, "mfmode" },
 #endif
 
     { NULL },
